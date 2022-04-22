bd4c8ba17f30612b2d14bb2c9e609ebde1fc6e49
Carlos Silva <88803987+guaxelo@users.noreply.github.com>
2022-04-21 22:24:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,49 +4,51 @@ var hh = 0;
 var mm = 0;
 var ss = 0;
 
-var tempo = 1000;//Quantos milésimos valem 1 segundo?
+var tempo = 1000;
 var cron;
+var click = 0
 
-//Inicia o temporizador
 function start() {
-    cron = setInterval(() => { timer(); }, tempo);
+    
+    if(click == 0){
+        cron = setInterval(() => { timer(); }, tempo);
+        click = 1
+    }
 }
 
-//Para o temporizador mas não limpa as variáveis
+
 function pause() {
     clearInterval(cron);
+    click = 0
 }
 
-//Para o temporizador e limpa as variáveis
+
 function reset() {
     clearInterval(cron);
     hh = 0;
     mm = 0;
     ss = 0;
+    click = 0
 
     document.getElementById('timer').innerText = '00:00:00';
 }
 
-//Faz a contagem do tempo e exibição
+
 function timer() {
-    ss++; //Incrementa +1 na variável ss
+    ss++
+    if (ss == 59) { 
+        ss = 0; 
+        mm++; 
 
-    if (ss == 59) { //Verifica se deu 59 segundos
-        ss = 0; //Volta os segundos para 0
-        mm++; //Adiciona +1 na variável mm
-
-        if (mm == 59) { //Verifica se deu 59 minutos
-            mm = 0;//Volta os minutos para 0
-            hh++;//Adiciona +1 na variável hora
+        if (mm == 59) {
+            mm = 0;
+            hh++;
         }
     }
 
-    //Cria uma variável com o valor tratado HH:MM:SS
     var format = (hh < 10 ? '0' + hh : hh) + ':' + (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss);
     
-    //Insere o valor tratado no elemento counter
     document.getElementById('timer').innerText = format;
 
-    //Retorna o valor tratado
     return format;
 }
